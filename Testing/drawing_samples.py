6e5c8e957e2d5a11ce96119532f8f1e239f5bd5e
Dave Josephsen <djosephsen@fastly.com>
2021-09-17 20:26:54 -0600

    foo

---
+++ Testing/drawing_samples.py
@@ -0,0 +1,13 @@
+"""
+This is a multi-line comment in a file I don't know what it does yet
+It'll probably have lots of bugs in it though.
+"""
+
+#foo and stuff
+
+import arcade
+arcade.open_window(600, 600, "Drawing Example")
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+arcade.start_render()
+arcade.finish_render()
+arcade.run()
